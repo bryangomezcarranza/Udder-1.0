40ec11d7b708dca9519fba7b0968f5347549dfc8
bryangomezcarranza <bryangomezcarranza@gmail.com>
2021-02-26 12:20:34 -0800

    Initial Commit

---
+++ Udder 1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder 1.0
+//
+//  Created by Bryan Gomez on 2/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
